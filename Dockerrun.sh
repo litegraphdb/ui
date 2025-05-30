74a9e2b80c8f5bdbd365f13556cae25c9caa195f
Joel Christner <joel.christner@gmail.com>
2025-05-29 23:40:35 -0700

    Docker updates

--- Dockerrun.sh
+++ Dockerrun.sh
@@ -4,21 +4,9 @@ fi
 
 echo Using image tag $IMG_TAG
 
-if [ ! -f "config.ts" ]
-then
-  echo Configuration file config.ts not found.
-  exit
-fi
-
-# Items that require persistence
-#   config.ts
-
-# Argument order matters!
-
 docker run \
   -p 3000:3000 \
   -t \
   -i \
   -e "TERM=xterm-256color" \
-  -v ./config.ts:/app/constants/config.ts \
   jchristn/litegraph-ui:$IMG_TAG
--- config.ts
+++
@@ -1,2 +0,0 @@
-export const liteGraphInstanceURL = 'http://localhost:8701/';
-export const globalToastId = 'global-toast';
